38a4ca8a6e1d4078d76f482232d724a8c762352b
Alisson Renan <renalima.rl46@gmail.com>
2022-12-08 14:55:34 -0300

    Making header, premium line and spacings

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,7 @@ import Hero from "./components/Hero/"
 import "./styleGlobal.css"
 function App() {
   return (
-    <div className="container">
       <Hero/>
-    </div>
   )
 }
 
--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,15 +1,20 @@
 import ButtonLink from "../ButtonLink/"
-
+import Header from "../Header/"
 export default function Hero(){
     return(
         <>
-            <header></header>
-            <div className="text-content center">
-                <h1>Lorem ipsum dolor sit amet, consectetur adipiscing.</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis fringilla velit ut tellus faucibus, vitae molestie ligula porta. Morbi imperdiet nunc ullamcorper lectus cursus.</p>
-                <ButtonLink content="Buy now" name="button primary" />
-                <ButtonLink content="See more" name="button secondary" />
+        <div className="back">
+            <Header/>
+            <div className="container">
+                <header></header>
+                <div className="text-content center">
+                    <h1>Lorem ipsum dolor sit amet, consectetur adipiscing.</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis fringilla velit ut tellus faucibus, vitae molestie ligula porta. Morbi imperdiet nunc ullamcorper lectus cursus.</p>
+                    <ButtonLink content="Buy now" name="button primary" />
+                    <ButtonLink content="See more" name="button secondary" />
+                </div>
             </div>
+        </div>
         </>
     )
 }

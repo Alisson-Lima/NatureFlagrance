b1783821eb7499987d40b5357fab8db899dc6330
Alisson Renan <renalima.rl46@gmail.com>
2022-12-11 15:20:10 -0300

    Making about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Hero from "./components/Hero/"
 import Points from "./components/Points"
 import Footer from "./components/Footer"
+import About from "./components/About"
 import "./styleGlobal.css"
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Hero/>
       <main className="main center">
         <Points />
+        <About />
       </main>
       <Footer />
     </>
---
+++ src/components/About/index.jsx
@@ -0,0 +1,21 @@
+export default function About(){
+    return (
+        <>
+            <div className="container flex" id="About">
+                <div className="about-img-company center">
+                    <img src="./assets/imgs/about-img.jpg" alt="Company image"  />
+                </div>
+                <div className="about-text-content center">
+                    <h2>Lorem ipsum dolor sit amet</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum deleniti quo incidunt rem cumque reprehenderit vero a omnis in aliquam. Officiis sequi repellendus enim sit quae, harum dolor ipsa consequuntur!</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum deleniti quo incidunt rem cumque reprehenderit vero a omnis in aliquam. Officiis sequi repellendus enim sit quae, harum dolor ipsa consequuntur!</p>
+                    <div className="about-social-icons flex">
+                        <a href="#" className="social"></a>
+                        <a href="#" className="social"></a>
+                        <a href="#" className="social"></a>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}

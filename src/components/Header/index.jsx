5768ac28cf3c16cb84563cc25b83bbe60e41bcf8
Alisson Renan <renalima.rl46@gmail.com>
2022-12-13 13:05:58 -0300

    Adding anchor menu link

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -15,7 +15,7 @@ export default function Header(){
                 </a>
                 <div className="menu flex">
                     <ul className="flex no-mobile">
-                        <li><a href="#">
+                        <li><a href="#Hero">
                             Home
                             <svg className="arrow-bottom" width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M1 1L7 7L1 13" stroke="#CCCDCE" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
@@ -27,7 +27,7 @@ export default function Header(){
                                 <path d="M1 1L7 7L1 13" stroke="#CCCDCE" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
                             </svg>
                         </a></li>
-                        <li><a href="#">
+                        <li><a href="#Faq">
                             Faq
                             <svg className="arrow-bottom" width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M1 1L7 7L1 13" stroke="#CCCDCE" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
